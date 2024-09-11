c3fcd455b050774cf006ed5c6be330fe2a580073
ShernOC <sherlynea1994@hotmail.com>
2024-09-11 05:01:19 +0300

    created classes

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    #we initialize it using the dunder method
+    def __init__(self,name,breed="Mutt"):
+        self.name = name 
+        self.breed = breed
+        
+        
+        
+        
+
+        
+        
+        
