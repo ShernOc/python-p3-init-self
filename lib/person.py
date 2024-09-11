4f47d87ca1afce801b24afdd130011d540cbae8b
ShernOC <sherlynea1994@hotmail.com>
2024-09-11 05:16:30 +0300

    edited

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,16 @@
 class Person:
     def __init__(self,name):
         self.name = name 
+    # to perform an action, and call it 
+    def names(self):
+        print(f"Hello {self.name}")
+    # def __str__(self) -> str:
+    #     return f"Person: {self.name}"
+        
+#create an object 
+sherlyne =Person("sherlyne")
+sherlyne.names() 
+
+print(sherlyne)
         
         
